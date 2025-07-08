e19e03e66aebd071cbe88e909fab05a43db28142
nhenriquez <nehrnqiuez116@gmail.com>
2025-07-07 22:54:10 -0400

    major changes to page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,55 @@
 import streamlit as st
-import pandas as pd
 
-st.title("🌌 What's Your Weight on Other Planets?")
+# Set page config
+st.set_page_config(
+    page_title="🌞 My Summer Solar System Coding Project 🚀",
+    page_icon="🌌",
+    layout="centered"
+)
 
-# Ask for user input
-# Ask for user inputTest
-#new
-earth_weight = st.number_input("Enter your weight on Earth (in pounds or kg):", min_value=0.0, step=1.0)
+# Title with emoji
+st.markdown("<h1 style='text-align: center; color: orange;'>🌞 My Summer Solar System Coding Project 🚀</h1>", unsafe_allow_html=True)
 
-# Gravitational factors relative to Earth
-gravity = {
-    "Mercury": 0.38,
-    "Venus": 0.91,
-    "Earth": 1.00,
-    "Mars": 0.38,
-    "Jupiter": 2.34,
-    "Saturn": 1.06,
-    "Uranus": 0.92,
-    "Neptune": 1.19,
-    "Pluto": 0.06  # Optional
-}
+# Fun welcome message
+st.markdown("""
+<div style='text-align: center; font-size: 20px;'>
+Welcome to my <span style='color: gold;'><b>summer journey</b></span> through space and code! 🌍💻
+</div>
+""", unsafe_allow_html=True)
 
-if earth_weight > 0:
-    results = {
-        planet: round(earth_weight * factor, 2)
-        for planet, factor in gravity.items()
-    }
+st.markdown("---")
 
-    df = pd.DataFrame.from_dict(results, orient='index', columns=["Your Weight"])
-    st.subheader("💫 Your Weight on Each Planet:")
-    st.dataframe(df)
+# Project intro
+st.markdown("""
+### 🌟 **Why I Built This Project**
+This summer, I'm combining **science** 🧪 and **computer programming** 👩‍💻 to:
+- Learn awesome facts about the **planets** in our **Solar System** 🪐
+- Build cool interactive **web apps** using **Streamlit**
+- Practice using **VS Code** and **GitHub** like real developers 🚀
+""")
 
-    st.bar_chart(df)
+st.markdown("""
+### 🛠️ **What You’ll Find Here**
+This project has multiple pages with fun tools and activities:
+- 🌍 **How old you'd be on other planets**
+- ⚖️ **Your weight on Jupiter or the Moon**
+- 🛰️ **Fun facts about each planet**
+- 🧑‍🚀 More coming soon as I learn!
+""")
 
-else:
-    st.info("Please enter a weight greater than 0.")
+st.markdown("""
+### 👨‍💻 **Tech I'm Using**
+- [Streamlit](https://streamlit.io/) for building apps 💡  
+- [GitHub](https://github.com/) to save and share my code 🌐  
+- [VS Code](https://code.visualstudio.com/) as my coding notebook 📓
+""")
 
+st.markdown("---")
+
+# Closing message
+st.markdown("""
+<div style='text-align: center; font-size: 18px; color: green;'>
+<b>Click the menu on the left to explore the universe with me!</b> 🌠<br>
+Let’s code and learn something cosmic together! 🛸
+</div>
+""", unsafe_allow_html=True)
